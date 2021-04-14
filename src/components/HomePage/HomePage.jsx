3cc56e00bb418a47b179008c673d7bb9b4359e89
Peter Trinh <peter@particlespace.com>
2021-04-14 02:18:24 -0500

    Initialize new home page

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+export default class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            
+        }
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div className="test">
+                Initial Home Page
+            </div>
+        )
+    }
+}
